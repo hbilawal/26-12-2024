21811f5298e20c9f14cb2a40f4d618845c645135
hingorobilawal <hingorobilawal786@gmail.com>
2024-12-27 09:28:45 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,34 @@
+// chapter no14-16
+
+// qno1
+// ans
+
+// var city0 = "karachi"
+// var city1 = "lahore"
+// var city2 = "islamabad"
+// var city3 = "quetta"
+// var city4 = "peshawer"
+
+
+// var selectedCities = "islamabad +" "+ quetta"
+
+// qno2
+// answer
+
+// var scoresofStudent = (320,230,480,120)
+// document.write("scoresofStudent")
+
+// var scoresofStudent = (120,230,320,480)
+// document.write("scoresofStudent")
+
+// qno3
+// ans
+
+// let studentName1 = "michael"
+// let studentName2 = "john"
+// let studentName3 = "tony"
+
+// var michael = +prompt(320)
+// var john = +prompt(230)
+// var tony = +prompt(480)
+
